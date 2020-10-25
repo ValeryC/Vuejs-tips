22c4fd64ff3a839f6f5355756dd9f5af13ab5109
Chin Valery <chinvalery@gmail.com>
2020-10-25 02:06:56 +0100

    addsome exemple

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <Home :listdata="items" />
+        <router-view />
+    </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  data() {
+      return { 
+          items:[         
+          {item: 'pencil', color:'blue'},
+          {item: 'pencil2', color:'red'},
+          {item: 'pencil3', color:'orange'},
+          {item: 'pencil4', color:'green'},
+          {item: 'pencil5', color:'brown'},
+          ],
+          }
+    }
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
